bb3423516eefc314e5e01a7eeb6228e786653d63
Myron <myronouyang@gmail.com>
2021-06-06 12:53:05 +0200

    works now

--- Game-o-lifo/game.js
+++ Game-o-lifo/game.js
@@ -4,27 +4,60 @@ class Game {
         this.dead = `#181818`;
         this.alive = `#2E8BC0`;
         this.size = 5;
-        for (let i = 1; i < this.grid.length-1; i++) {
+        for (let i = 1; i < this.grid.length - 1; i++) {
             this.grid[i] = new Array(102);
-            for (let j = 1; j < this.grid[i].length-1; j++) {
+            for (let j = 1; j < this.grid[i].length - 1; j++) {
                 this.grid[i][j] = this.randomStart(.95)
             }
         }
     }
 
-    roundPass
+    checkNeighbours(row, col) {
+        let numAlive = 0
+        for (let i = row - 1; i <= row + 1; i++) {
+            if (typeof (this.grid[i]) !== "undefined") {
+                for (let j = col - 1; j <= col + 1; j++) {
+                    if (typeof (this.grid[i][j]) !== "undefined" && !(i == row && j == col) && this.grid[i][j] > 0) {
+                        numAlive += 1;
+                    }
+                }
+            }
+        }
+
+        if (this.grid[row][col] == 1) {
+            if (numAlive < 2) {
+                this.grid[row][col] = 0
+            } else if (numAlive > 3) {
+                this.grid[row][col] = 0
+            }
+        } else {
+            if (numAlive == 3) {
+                this.grid[row][col] = 1
+            }
+        }
+    }
+
+    roundPass() {
+        for (let i = 1; i < this.grid.length - 1; i++) {
+            for (let j = 1; j < this.grid[i].length - 1; j++) {
+                this.checkNeighbours(i, j);
+            }
+        }
+        this.fillCanvas()
+    }
+
     randomStart(prob) {
         let res = 0;
         let ran = Math.random();
-        if(ran > prob){
+        if (ran > prob) {
             res = 1;
         }
         return res
     }
 
     fillCanvas() {
-        for (let i = 1; i < this.grid.length -1; i++) {
-            for (let j = 1; j < this.grid[i].length-1; j++) {
+        for (let i = 1; i < this.grid.length - 1; i++) {
+            for (let j = 1; j < this.grid[i].length - 1; j++) {
                 if (this.grid[i][j] > 0) {
                     ctx.fillStyle = this.alive;
                 } else {
@@ -35,7 +68,3 @@ class Game {
         }
     }
 }
-
-
-// let game = new Game()
-// console.log("ding");
--- Game-o-lifo/index.js
+++ Game-o-lifo/index.js
@@ -2,31 +2,9 @@ const canvas = document.querySelector("#gamefield")
 const ctx = canvas.getContext("2d")
 
 let game = new Game()
-game.fillCanvas()
-// dead_color = `#181818`;
-// alive_color = `#2E8BC0`;
-// ctx.fillStyle = alive_color;
-// ctx.fillRect(0, 0, 5, 5);
-// ctx.fillRect(0, 5, 5, 5);
-// ctx.fillRect(495, 10, 5, 5);
-// ctx.fillRect(5, 5, 5, 5);
-// ctx.fillRect(10, 10, 5, 5);
 
-// const game = new GameOfLife()
-// game.gameSetUp()
-
-// window.onload = () => {
-
-//     document.querySelector("#start-random").addEventListener("click", () => {
-//         game.arrayRandomize();
-//         game.fillArray();
-//         window.setInterval(() => {
-//             game.runGame();
-//         }, 300)
-//     })
-
-//     document.querySelector("#stop").addEventListener("click", () => {
-//         game.gameSetUp();
-//     })
-
-// }
+window.onload = () => {
+    window.setInterval(() => {
+        game.roundPass()
+    }, 50)
+}
